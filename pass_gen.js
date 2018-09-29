9d2d2d1bff0ad7e4171215f4f927f006c6e503f0
Brian Welch <myself@brianwelch.se>
2018-09-29 18:05:57 +0200

    small typo fix

--- pass_gen.js
+++ pass_gen.js
@@ -2,7 +2,7 @@ var size = 30;
 var lib = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz@%+#!$?~-_";
 var code = "";
 while (size > 0) {
-  code += (lib[Math.floor(Math.random()*library.length)]);
+  code += (lib[Math.floor(Math.random()*lib.length)]);
   size--;
 }
 console.log(code);
